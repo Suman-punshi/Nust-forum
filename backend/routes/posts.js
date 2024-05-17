dba7f99ac94734d9c2d7566c5f0d59fad39df400
Hamza Riaz <hamzariaz999@gmail.com>
2024-05-17 19:19:50 +0500

    navbar updated

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('../models/Users');
+const User = require('../models/usermodel');
 
 
 // Route to insert data into the MongoDB database
